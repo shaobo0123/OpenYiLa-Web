c8e2217db4bd489e27bd3dafe6c7567754d6fb7d
shaobo <1036566628@qq.com>
2026-07-05 14:09:00 +0800

    Upgrade uni-app toolchain to 5.14

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,4 +4,12 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        silenceDeprecations: ["legacy-js-api", "import", "global-builtin", "if-function"],
+      },
+    },
+  },
 });
